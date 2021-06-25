6a6a979c9e4684c9349aeb07365766651a1b197b
Sebastiano Zagatti <sebastiano.zagatti@gmail.com>
2021-06-25 23:50:52 +0200

    checked the class and addedd comments

--- c++/include/node.hpp
+++ c++/include/node.hpp
@@ -1,6 +1,10 @@
 #ifndef node_hpp
 #define node_hpp
 
+// ============================== NODE ===============================
+//
+// Structure encoding the concept of a node in a binary search tree.
+
 template<typename key_type, typename value_type, typename comparison_type>
 struct bst<key_type, value_type, comparison_type>::node{
 
@@ -16,19 +20,16 @@ struct bst<key_type, value_type, comparison_type>::node{
   // each node stores a key-value pair
   std::pair<const key_type, value_type> pair;
 
-  // ctors
-  node() : parent{nullptr}, left{nullptr}, right{nullptr}, pair{} {} //create an empty node, all pointers are set to nullptr
-  
+  // ctor
+  node() noexcept: parent{nullptr}, left{nullptr}, right{nullptr}, pair{} {} //create an empty node, all pointers are set to nullptr
+
   // copy ctor with parent and pair
-  node(node* p, const std::pair<const key_type, value_type>& data) :
+  node(node* p, const std::pair<const key_type, value_type>& data) noexcept:
     parent{p}, right{nullptr}, left{nullptr}, pair{data}{}
   
   // move ctor with parent and pair
-  node(node* p, std::pair<const key_type, value_type>&& data) :
+  node(node* p, std::pair<const key_type, value_type>&& data) noexcept:
     parent{p}, right{nullptr}, left{nullptr}, pair{std::move(data)}{}
-
-  // destructor
-  ~node() = default;
 
 };
 
